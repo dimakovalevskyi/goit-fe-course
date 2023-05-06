aacbbc3c51d4e0bd7bce3e2c5aba541073e28a0b
mosby <dimakovalevskyi@gmail.com>
2023-05-06 20:59:26 +0300

    add tasks for lesson 11

---
+++ lessons/lesson-11/script.js
@@ -0,0 +1,37 @@
+// - Урок 11
+
+// 1. Створити список технологій що вивчаються на курсі Fullstack за допомогою js
+    
+//     ```jsx
+//     const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+//     ```
+    
+
+// Показати як це робиться через innerHTML:map/reduce.
+
+// 1. Створити кілька кнопок на основі масива з обєктами використовуючи createElement. map+append(...items)/map+forEach/forEach
+
+// ```jsx
+// const colors = [
+//     {
+//         label: "red",
+//         color: "#FF0000",
+//     },
+//     {
+//         label: "green",
+//         color: "#00FF00",
+//     },
+//     {
+//         label: "blue",
+//         color: "#0000FF",
+//     },
+//     {
+//         label: "yellow",
+//         color: "#FFFF00",
+//     },
+// ];
+// ```
+
+// 1. Створити сайт з такою розміткою використовуючи лише JS в html файлі має бути пусто
+
+// see example.html
